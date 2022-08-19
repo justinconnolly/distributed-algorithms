ebeeaf36132dfccc6c8603ebf366392292d7a1fa
Justin Connolly <justin.connolly@gmail.com>
2022-08-19 09:44:27 -0400

    Mistake in get_left was messing up everything else

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -1,24 +1,24 @@
-
+from typing import List
 class PriorityQueue:
     def __init__(self) -> None:
         self.a = []
         self.d = {}
     
-    def get_parent(self, i):
+    def get_parent(self, i: int) -> None:
         return i // 2
     
-    def get_left(self, i):
-        return (i * 2)
+    def get_left(self, i: int) -> None:
+        return (i * 2) + 1
 
-    def get_right(self, i):
+    def get_right(self, i: int) -> None:
         return 2 * (i + 1)
 
-    def add(self, x):
+    def add(self, x: List[int]) -> None:
         self.a.append(x)
         self.d[x[1]] = len(self.a) - 1
         self.bubble_up(len(self.a) - 1)
 
-    def bubble_up(self, i):
+    def bubble_up(self, i: int) -> None:
         p = self.get_parent(i)
         while i > 0 and self.a[i] < self.a[p]:
             self.a[i], self.a[p] = self.a[p], self.a[i]
@@ -26,14 +26,17 @@ class PriorityQueue:
             i = p
             p = self.get_parent(i)
 
-    def get_min(self):
+    def get_min(self) -> List[int]:
         x = self.a[0]
         del self.d[x[1]]
-        self.a[0] = self.a.pop()
-        self.trickle_down(0)
+        if len(self.a) > 1:
+            self.a[0] = self.a.pop()
+            self.trickle_down(0)
+        else:
+            self.a.pop()
         return x
 
-    def trickle_down(self, i):
+    def trickle_down(self, i: int) -> None:
         while i >= 0:
             j = -1
             r = self.get_right(i)
@@ -52,27 +55,31 @@ class PriorityQueue:
                 self.d[self.a[j][1]], self.d[self.a[i][1]] = self.d[self.a[i][1]], self.d[self.a[j][1]]
             i = j
 
-    def decrease_key(self, item, key):
-        i = self.d[item]
-        self.a[i][0] = key
+    def decrease_key(self, node: int, weight: int) -> None:
+        i = self.d[node]
+        self.a[i][0] = weight
         self.bubble_up(i)
+
+    def contains(self, node: int) -> bool:
+        return node in self.d
+
 
 if __name__ == "__main__":
     arr = [
         [1,4],
         [5,1],
         [3,0],
-        [5,2],
+        [6,2],
         [2,10]
         ]
     pq = PriorityQueue()
 
     for item in arr:
         pq.add(item)
-
-    print(pq.a)
-
-    pq.a[2][0] = 0
-    pq.bubble_up(2)
+        print(pq.a)
+    pq.decrease_key(2,0)
     print(pq.a)
     print(pq.d)
+    for i in arr:
+        print(pq.get_min())
+        print(pq.a)
