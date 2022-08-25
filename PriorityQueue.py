76f4a03abd6288b2db29dd4c219940bf6ccb7428
Justin Connolly <justin.connolly@gmail.com>
2022-08-25 12:12:36 -0400

    Fixed index dict for O(1) contains and O(log n) decrease_key (rather than O(n) for both)

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -4,7 +4,7 @@ from typing import List
 class PriorityQueue:
     def __init__(self) -> None:
         self.a = []
-        # self.d = {}
+        self.d = {}
     
     def get_parent(self, i: int) -> int:
         return (i - 1) // 2
@@ -17,25 +17,28 @@ class PriorityQueue:
 
     def add(self, x, weight) -> None:
         self.a.append([weight, x])
-        # self.d[x] = len(self.a) - 1
+        self.d[x] = len(self.a) - 1
         self.bubble_up(len(self.a) - 1)
 
     def bubble_up(self, i: int) -> None:
         p = self.get_parent(i)
         while i > 0 and self.a[i] < self.a[p]:
             self.a[i], self.a[p] = self.a[p], self.a[i]
-            # self.d[self.a[i][1]], self.d[self.a[p][1]] = self.d[self.a[p][1]], self.d[self.a[i][1]]
+            self.d[self.a[i][1]], self.d[self.a[p][1]] = self.d[self.a[p][1]], self.d[self.a[i][1]]
             i = p
             p = self.get_parent(i)
 
     def get_min(self) -> List[int]:
         x = self.a[0]
-        # del self.d[x[1]]
+        del self.d[x[1]]
         if len(self.a) > 1:
             self.a[0] = self.a.pop()
+            self.d[self.a[0][1]] = 0
             self.trickle_down(0)
         else:
-            self.a.pop()
+            val = self.a.pop()
+        print(self.a)
+        print(self.d)
         return x
 
     def trickle_down(self, i: int) -> None:
@@ -54,21 +57,21 @@ class PriorityQueue:
                     j = l
             if j >= 0:
                 self.a[j], self.a[i] = self.a[i], self.a[j]
-                # self.d[self.a[j][1]], self.d[self.a[i][1]] = self.d[self.a[i][1]], self.d[self.a[j][1]]
+                self.d[self.a[j][1]], self.d[self.a[i][1]] = self.d[self.a[i][1]], self.d[self.a[j][1]]
             i = j
 
     def decrease_key(self, node: int, weight: int) -> None:
-        # i = self.d[node]
-        for index, pair in enumerate(self.a):
-            if pair[1] == node:
-                i = index
-                break
+        i = self.d[node]
+        # for index, pair in enumerate(self.a):
+        #     if pair[1] == node:
+        #         i = index
+        #         break
         
         self.a[i][0] = weight
         self.bubble_up(i)
 
     def contains(self, node: int) -> bool:
-        # return node in self.d
+        return node in self.d
         for i, pair in enumerate(self.a):
             if pair[1] == node:
                 return True
@@ -99,7 +102,7 @@ if __name__ == "__main__":
         print(pq.a)
     pq.decrease_key(2,0)
     print(pq.a)
-    # print(pq.d)
+    print(pq.d)
     for i in arr:
         print(pq.get_min())
         print(pq.a)
